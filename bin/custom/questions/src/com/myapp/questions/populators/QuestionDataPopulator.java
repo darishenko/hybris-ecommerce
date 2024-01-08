5a03d978cc1a888684cb0d880adff666be82a334
DARYA <89320281+darishenko@users.noreply.github.com>
2024-01-08 23:09:21 +0300

    Task 13: Business process

--- bin/custom/questions/src/com/myapp/questions/populators/QuestionDataPopulator.java
+++ bin/custom/questions/src/com/myapp/questions/populators/QuestionDataPopulator.java
@@ -4,16 +4,20 @@ import com.myapp.questions.data.QuestionData;
 import com.myapp.questions.model.QuestionModel;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import org.apache.commons.lang.StringUtils;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class QuestionDataPopulator implements Populator<QuestionModel, QuestionData> {
     @Override
-    public void populate(@Nonnull QuestionModel questions, @Nonnull QuestionData questionData)
-            throws ConversionException {
-        questionData.setQuestionCustomerName(questions.getQuestionCustomer().getName());
-        questionData.setQuestion(questions.getQuestion());
-        questionData.setAnswerCustomerName(questions.getAnswerCustomer().getName());
-        questionData.setAnswer(questions.getAnswer());
+    public void populate(@Nonnull QuestionModel question, @Nonnull QuestionData questionData) throws ConversionException {
+        questionData.setQuestionCustomerName(question.getQuestionCustomer().getName());
+        questionData.setQuestion(question.getQuestion());
+
+        if (Objects.nonNull(question.getAnswerCustomer()) && StringUtils.isNotBlank(question.getAnswer())) {
+            questionData.setAnswerCustomerName(question.getAnswerCustomer().getName());
+            questionData.setAnswer(question.getAnswer());
+        }
     }
 }
---
+++ bin/custom/training/trainingcore/src/de/hybris/training/core/daos/impl/DefaultQuestionDao.java
@@ -0,0 +1,39 @@
+package de.hybris.training.core.daos.impl;
+
+import de.hybris.training.core.daos.QuestionDao;
+import com.myapp.questions.model.QuestionModel;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
+import de.hybris.platform.servicelayer.search.FlexibleSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component("questionDao")
+public class DefaultQuestionDao implements QuestionDao {
+    private static final String QUERY_SELECT_ALL_QUESTIONS = "SELECT {p:"
+            + QuestionModel.PK + "} FROM {"
+            + QuestionModel._TYPECODE + " AS p} ";
+    private static final String QUERY_SELECT_NEW_QUESTIONS = QUERY_SELECT_ALL_QUESTIONS
+            + "WHERE {p:" + QuestionModel.CREATIONTIME + "} > ?" + QuestionModel.CREATIONTIME ;
+
+    @Autowired
+    private FlexibleSearchService flexibleSearchService;
+
+    @Override
+    public List<QuestionModel> getQuestions() {
+        final FlexibleSearchQuery query = new FlexibleSearchQuery(QUERY_SELECT_ALL_QUESTIONS);
+
+        return flexibleSearchService.<QuestionModel>search(query).getResult();
+    }
+
+    @Override
+    public List<QuestionModel> getQuestionsCreatedAfterTime(Date time) {
+        final FlexibleSearchQuery query = new FlexibleSearchQuery(QUERY_SELECT_NEW_QUESTIONS);
+
+        query.addQueryParameter(QuestionModel.CREATIONTIME, time);
+
+        return flexibleSearchService.<QuestionModel>search(query).getResult();
+    }
+}
